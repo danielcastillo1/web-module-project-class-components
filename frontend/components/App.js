8c682a2916b86f4e4b1ea697a78e3dddfa3cda81
Daniel Castillo <danielcastillo89@icloud.com>
2023-05-22 19:34:07 -0500

    finished module 1

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,25 +1,68 @@
 import React from 'react'
 import TodoList from './TodoList';
+import Form from './Form';
 
 
 export default class App extends React.Component {
-constructor() {
-  super();
-  this.state = {
-    todos: [
-      {
-        name: 'Organize Garage',
-        id: 1528817077286, // could look different, you could use a timestamp to generate it
-        completed: false
-      },
-      {
-        name: 'Bake Cookies',
-        id: 1528817084358,
-        completed: false
-      }
-    ]
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
   }
-}
+
+  handleAdd = (task) => {
+
+    const newTodo = {
+      task: task,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo]
+    });
+  }
+
+  handleClear = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(todo => {
+        return (todo.completed === false);
+      })
+    });
+  }
+
+  handleToggle = () => {
+
+    const clickedId = 1528817084358
+
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(todo => {
+        if (todo.id === clickedId) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        } else {
+          return todo
+        }
+      })
+    });
+
+  }
 
 
   render() {
@@ -28,17 +71,12 @@ constructor() {
     return (
       <div>
         Todo App
-      
-      <TodoList todos={todos}/>
-      
-      <form>
-        <input />
-        <button>Add a Todo!</button>
-      </form>
 
-      <button>Clear</button>
+        <TodoList todos={todos} />
+        <Form handleAdd={this.handleAdd} />
+        <button onClick={this.handleClear}>Clear</button>
       </div>
-    
+
     )
   }
 }
